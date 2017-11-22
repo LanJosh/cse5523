33a7f2565885143c445baf52418f8c9a981c1c9f
Joshua <josh.lan9@gmail.com>
2017-11-22 13:18:04 -0500

    Fixed up train script, added dummy debug step

--- train.py
+++ train.py
@@ -8,22 +8,24 @@ import numpy as np
 
 import fcn
 
-# TODO @Thaight27 this should result in an array of tuples where the first 
-# element is the image to mask and the second element is the ground truth mask
-data = preprocessing.get_data()
 
+# Placeholder for the inputs
 x = tf.placeholder(tf.float32, [None, 227, 227, 3])
+
+# Placeholder for the image mask
 y_ = tf.placeholder(tf.float32, [None, 227, 227, 3])
+
+xd = np.ones((1, 227, 227, 3))
+yd = np.zeros((1, 227, 227, 3))
+
 with slim.arg_scope(fcn.alexnet_v2_arg_scope()):
-  y_conv = alexnet_v2(x)
+  y_conv = fcn.alexnet_v2(x)
+
 loss = tf.squared_difference(y_, y_conv)
 losses = tf.reduce_mean(loss)
 train_step = tf.train.AdamOptimizer(1e-4).minimize(losses)
 
-# TODO Implement some saving of model parameters for writeup, print the loss 
-# for debugging
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
-  for im,mask in data:
-      train_step.run(feed_dict={x: im, y: mask})
+  train_step.run(feed_dict={x: xd, y_: yd})
 
